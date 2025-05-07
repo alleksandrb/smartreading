b3772671da4fd3deb0102ce2f6b6a47be1b5480c
alleksandrb <bashtein@gmail.com>
2025-05-07 19:55:22 +0300

    Add authentication routes and organize API endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TaskController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Public routes
+Route::post('login', [AuthController::class, 'login']);
+
+// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    
     Route::get('tasks', [TaskController::class, 'index']);
     Route::post('tasks', [TaskController::class, 'store']);
     Route::get('tasks/{taskId}', [TaskController::class, 'show']);
